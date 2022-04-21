692f7d31830dfd5fe045a8209c34653403b5441f
Angelina <angelina.kuptcova@gmail.com>
2022-04-21 15:11:57 +0300

    added loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import loader from '../images/loader.gif'
+
+const Loader = () => {
+    return (
+        <div className={`loader`}>
+            <img src={loader} alt="loading" />
+        </div>
+    );
+}
+
+export default Loader;
--- src/components/OperationsInfo.tsx
+++ src/components/OperationsInfo.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { getDocs } from "firebase/firestore"
 import { formatDateTime } from "../utils/utils"
 import { operationsCollectionRef } from "../App"
+import Loader from "./Loader";
 
 type OperationsType = {
     id: string
@@ -13,16 +14,23 @@ type OperationsType = {
 
 const OperationsInfo = () => {
     const [operations, setOperations] = useState<OperationsType[]>([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const getData = async () => {
+            setLoading(true)
             const data = await getDocs(operationsCollectionRef)
             // @ts-ignore
             setOperations(data.docs.map((doc) => ({...doc.data(), id: doc.id}) ))
+            setLoading(false)
         }
 
         getData()
     }, [])
+
+    if (loading) {
+        return <Loader />
+    }
 
     return  (
         <div className="info">
